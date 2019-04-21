f439552a8e2c56ec29606f5a115b55f75d399152
Jorge Verdugo <jorgeverdugoch@gmail.com>
2019-04-21 01:44:13 -0400

    Función para ingresar un grafo

--- main.rb
+++ main.rb
@@ -1,1 +1,26 @@
-puts "Esto lo escribio el mapache"
+def ingresarGrafo
+    puts "Ingrese el número de vertices: "
+    vertices = gets.to_i
+
+    if vertices > 26 then
+        puts "Plz no"
+    else
+        seguir = true
+
+        while true do
+            
+            vertices.times do |n|
+                etiqueta = (97 + n).chr
+                puts "Vertice " + etiqueta;
+            end
+
+            inicio = gets
+            
+
+        end
+    end
+
+    
+end
+
+ingresarGrafo
