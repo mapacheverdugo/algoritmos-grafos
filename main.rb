1c7dba2ae86688eb70c3d1c477e4a399484fb4f4
Spipe <fuerte.felipe@gmail.com>
2019-04-21 01:43:09 -0400

    Clase Arista

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
-puts "Ingrese su nombre:"
-nombre = gets
-nombre.chomp
-puts "Hola"+nombre+"."
+class Arista
+    def initialize(verticeI,verticeF,peso)
+        @VerticeI=verticeI
+        @VerticeF=verticeF
+        @Peso=peso
+    end
+    attr_reader :VerticeI, :VerticeF, :Peso
+end    
+
