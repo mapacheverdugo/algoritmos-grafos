b6fbcd585ac0be964c509cb076d4bd1cdaceb7c8
Jorge Verdugo <jorgeverdugoch@gmail.com>
2019-04-21 12:09:29 -0400

    Se conecta el menú con ingresarGrafo

--- main.rb
+++ main.rb
@@ -1,19 +1,13 @@
-class Arista
-    def initialize(verticeI,verticeF,peso)
-        @VerticeI=verticeI
-        @VerticeF=verticeF
-        @Peso=peso
-    end
-    attr_reader :VerticeI, :VerticeF, :Peso
-end
+require_relative "Arista"
 
 def textoMenu
     #Esta función sólo mostrará las opciones del menú principal
     puts "Menú principal"
-    puts "1) Mostrar matriz de adyacencia"
-    puts "2) Verificar si un grafo es conexo"
-    puts "3) Cantidad de caminos entre vértices"
-    puts "4) Dijkstra"
+    puts "1) Configurar grafo"
+    puts "2) Mostrar matriz de adyacencia"
+    puts "3) Verificar si un grafo es conexo"
+    puts "4) Cantidad de caminos entre vértices"
+    puts "5) Dijkstra"
     puts "0) Salir\n\n"
 end
 
@@ -24,48 +18,6 @@ def limpiar
       system 'clear'
     end
 end
-
-def menu
-    salir = false
-    opcion = nil
-
-    while salir!=true do
-        #limpiar #Esta función sirve para "limpiar" la consola
-        textoMenu
-        opcion = gets
-
-        case opcion.to_i
-            when 1
-                puts "Esta es la opción 1\n\n"
-                #Aquí va la función de la matriz de adyacencia
-            when 2
-                puts "Esta es la opción 2\n\n"
-                #Aquí va la función del grafo conexo
-            when 3
-                puts "Esta es la opción 3\n\n"
-                #Aquí va la función de cantidad de caminos
-            when 4
-                puts "Esta es la opción 4\n\n"
-                #Aquí va la función del Dijkstra
-            when 0
-                puts "El programa finalizará"
-                salir = true
-            else
-                puts "La opción no es válida\n\n"
-        end
-    end
-end
-
-def main
-    puts "Grafos y lenguajes formales\n21041|INFB8061-1\n\nTrabajo 1: Matrices de Adyacencia\n"
-    puts "Integrantes:\n"
-    puts "- Felipe Flores Vivanco\n- Andrés Mella\n- Jorge Verdugo Chacón\n- Javiera Vergara Navarro\n\n"
-    menu
-end
-main
-###
-# Aquí finaliza la presentación y el menú
-###
 
 def ingresarGrafo
     puts "Ingrese el número de vertices: "
@@ -111,4 +63,49 @@ def ingresarGrafo
     end
 end
 
-ingresarGrafo
+def menu
+    salir = false
+    opcion = nil
+
+    while salir!=true do
+        #limpiar #Esta función sirve para "limpiar" la consola
+        textoMenu
+        opcion = gets
+
+        case opcion.to_i
+            when 1
+                
+                ingresarGrafo
+            when 2
+                puts "Esta es la opción 2\n\n"
+                #Aquí va la función de la matriz de adyacencia
+            when 3
+                puts "Esta es la opción 3\n\n"
+                #Aquí va la función del grafo conexo
+            when 4
+                puts "Esta es la opción 4\n\n"
+                #Aquí va la función de cantidad de caminos
+            when 5
+                puts "Esta es la opción 5\n\n"
+                #Aquí va la función del Dijkstra
+            when 0
+                puts "El programa finalizará"
+                salir = true
+            else
+                puts "La opción no es válida\n\n"
+        end
+    end
+end
+
+def main
+    puts "Grafos y lenguajes formales\n21041|INFB8061-1\n\nTrabajo 1: Matrices de Adyacencia\n"
+    puts "Integrantes:\n"
+    puts "- Felipe Flores Vivanco\n- Andrés Mella\n- Jorge Verdugo Chacón\n- Javiera Vergara Navarro\n\n"
+    menu
+end
+
+###
+# Aquí finaliza la presentación y el menú
+###
+
+main
