5fc112f33c416d1bea535c4fa858a3e4ef321ff6
Jorge Verdugo <jorgeverdugoch@gmail.com>
2019-04-21 02:08:49 -0400

    Se implementa la clase Arista

---
+++ Arista.rb
@@ -0,0 +1,8 @@
+class Arista
+    def initialize(verticeI, verticeF, peso)
+        @VerticeI=verticeI
+        @VerticeF=verticeF
+        @Peso=peso
+    end
+    attr_reader :VerticeI, :VerticeF, :Peso
+end
--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
+class Arista
+    def initialize(verticeI,verticeF,peso)
+        @VerticeI=verticeI
+        @VerticeF=verticeF
+        @Peso=peso
+    end
+    attr_reader :VerticeI, :VerticeF, :Peso
+end
+
 def ingresarGrafo
     puts "Ingrese el número de vertices: "
     vertices = gets.to_i
@@ -6,30 +15,42 @@ def ingresarGrafo
         puts "Plz no"
     else
         seguir = true
+        aristas = []
 
-        while true do
+        while seguir do
             
             vertices.times do |n|
                 etiqueta = (97 + n).chr
-                puts "Vertice " + etiqueta;
+                puts "(#{n + 1}) Vertice " + etiqueta;
             end
+            puts "(0) Dejar de ingresar aristas"
 
-            inicio = gets
-            
+            puts "Ingrese el vertice de inicio"
+            inicio = gets.to_i
 
+            if inicio == 0 then
+                seguir = false
+            else 
+                puts "Ingrese el vertice de termino"
+                fin = gets.to_i
+
+                puts "Ingrese el peso de la arista"
+                peso = gets.to_i
+                arista = Arista.new(inicio, fin, peso)
+                aristas.push(arista)
+            end
+        end
+
+        for arista in aristas
+            puts "----"
+            puts "Inicio: #{arista.VerticeI}"
+            puts "Fin: #{arista.VerticeF}"
+            puts "Peso: #{arista.Peso}"
+            puts "----"
         end
     end
 
     
-end
-
-class Arista
-    def initialize(verticeI,verticeF,peso)
-        @VerticeI=verticeI
-        @VerticeF=verticeF
-        @Peso=peso
-    end
-    attr_reader :VerticeI, :VerticeF, :Peso
 end
 
 ingresarGrafo
