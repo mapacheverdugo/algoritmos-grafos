72fd15d5c2493753bc7defe5686b641344f48b15
Spipe <fuerte.felipe@gmail.com>
2019-04-24 03:19:17 -0400

    Matriz de adyacencia -rustica-

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 require_relative "Arista"
+require 'matrix'
+
+#variable global (Contiene cantidad de vertices)
+$vertices
+#Variable global (Lista de aristas)
+$aristas=[]
 
 def textoMenu
     #Esta función sólo mostrará las opciones del menú principal
@@ -21,17 +27,17 @@ end
 
 def ingresarGrafo
     puts "Ingrese el número de vertices: "
-    vertices = gets.to_i
+    $vertices = gets.to_i
 
-    if vertices > 26 then
+    if $vertices > 26 then
         puts "Plz no"
     else
         seguir = true
-        aristas = []
+        
 
         while seguir do
             
-            vertices.times do |n|
+            $vertices.times do |n|
                 etiqueta = (97 + n).chr
                 puts "(#{n + 1}) Vertice " + etiqueta;
             end
@@ -39,7 +45,7 @@ def ingresarGrafo
 
             puts "Ingrese el vertice de inicio"
             inicio = gets.to_i
-
+            
             if inicio == 0 then
                 seguir = false
             else 
@@ -49,11 +55,11 @@ def ingresarGrafo
                 puts "Ingrese el peso de la arista"
                 peso = gets.to_i
                 arista = Arista.new(inicio, fin, peso)
-                aristas.push(arista)
+                $aristas.push(arista)
             end
         end
 
-        for arista in aristas
+        for arista in $aristas
             puts "----"
             puts "Inicio: #{arista.VerticeI}"
             puts "Fin: #{arista.VerticeF}"
@@ -77,7 +83,12 @@ def menu
                 
                 ingresarGrafo
             when 2
-                puts "Esta es la opción 2\n\n"
+
+                matriz($vertices)
+                puts"Matriz de Adyacencia\n\n"
+                p $Matriz
+                p $Matriz**2
+                puts"\n"
                 #Aquí va la función de la matriz de adyacencia
             when 3
                 puts "Esta es la opción 3\n\n"
@@ -108,4 +119,55 @@ end
 # Aquí finaliza la presentación y el menú
 ###
 
+
+
+######################
+# Matriz de Adyasencia
+######################
+
+#Valor Global que contiene la Matriz de adyacencia
+$Matriz
+$ArrayM=[]
+
+#Crea una lista con las aristas igual al numero comparado
+def EncVertice(comparar)
+    numeros=[]
+    cont=0
+    while(cont<$aristas.length())
+        vertice=$aristas[cont]
+        if(comparar==vertice.VerticeI)
+            numeros.push(vertice.VerticeF)
+        elsif(comparar==vertice.VerticeF)
+            numeros.push(vertice.VerticeI)
+        end
+        cont=cont+1
+    end
+    numeros.sort()
+    return numeros
+end
+
+#Crea la matriz al comparar los datos obtenidos de la lista creada por la funcion anterior
+def matriz (numero)
+    $ArrayM=[]
+    for i in (0..numero-1)
+        $Fila=[]
+        $Comp=EncVertice(i+1)
+        cont=0
+        for j in (0..numero-1)
+            if(j+1==$Comp[cont])
+                $Fila.push(1)
+                cont=cont+1
+            else
+                $Fila.push(0)
+            end
+        end
+        $ArrayM.push($Fila)
+    end    
+    $Matriz= Matrix[*$ArrayM]                  
+end
+
+
+###############################
+
 main
+
